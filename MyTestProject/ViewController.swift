1efc0a775d8d3741d11057c0de3becf7d40ff2d2
Ankit Jain <ankitjain0409.mca@gmail.com>
2019-07-06 11:15:23 +0530

    my ios first commit

---
+++ MyTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTestProject
+//
+//  Created by Ankit Jain on 06/07/19.
+//  Copyright © 2019 Ankit Jain King of iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
